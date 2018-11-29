6d1f44a0c9bc8f42c1a1c3f997b173136512371a
Philip Dumaresq <phdumaresq@gmail.com>
2018-11-28 22:35:03 -0500

    Finished the test cases for separate chaining

--- src/a4_40082638_40042586/MyHashTable.java
+++ src/a4_40082638_40042586/MyHashTable.java
@@ -3,6 +3,7 @@ package a4_40082638_40042586;
 public abstract class MyHashTable {
 	public abstract int size();
 	public abstract boolean isEmpty();
+	public abstract boolean isFull();
 	public abstract Element get(int key);
 	public abstract Element put(int key, String value);
 	public abstract Element remove(int key);
